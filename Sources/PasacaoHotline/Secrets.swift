0dc6619e07555d04ab58924a4288cd32421d7b2b
Jayco Bea <jcobea.dev@gmail.com>
2025-04-20 22:00:51 +0800

    Add sample data

--- Sources/PasacaoHotline/Secrets.swift
+++ Sources/PasacaoHotline/Secrets.swift
@@ -8,7 +8,6 @@
 import Foundation
 
 enum Secrets {
-    static let projectURL = URL(string: "https://lvkdolhbctktrygamphh.supabase.co")!
-        
-    static let apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2a2RvbGhiY3RrdHJ5Z2FtcGhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUwNjY0NDEsImV4cCI6MjA2MDY0MjQ0MX0.lQrhZ3nMnNqGqnckqpHjpbOOCiuTDrNm0oTP9RZv2yE"
+    static let projectURL = URL(string: "https://your-project.supabase.co")!
+    static let apiKey = "<your-api-key-here>"
 }
